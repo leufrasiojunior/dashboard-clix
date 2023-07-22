675bdf55a01896fcd5cb94c1632bc44b7d315e84
leonald <leufrasiojunior@gmail.com>
2023-07-22 15:29:52 -0300

    alteração de post

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import HttpPost from './Routes/HttpPost'
 
 function App() {
   const Data = {
     login: "leonald.junior",
     senha: "Leo!@#2034"
   }
-  fetch("https://api.2clix.com.br/v3/Usuario/login",
-    {
-      method: "POST",
-      // mode: "no-cors",
-      headers: { 'Content-Type': 'application/json' },
-      body: {
-        "login": "leonald.junior",
-        "senha": "Leo!@#2034"
-      }
-    })
-    .then((resp) => { resp.json().then((result) => { console.warn("result", result) }) })
+  // fetch("https://api.2clix.com.br/v3/Usuario/login",
+  //   {
+  //     method: "POST",
+  //     // mode: "no-cors",
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: {
+  //       "login": "leonald.junior",
+  //       "senha": "Leo!@#2034"
+  //     }
+  //   })
+  //   .then((resp) => { resp.json().then((result) => { console.warn("result", result) }) })
 
   return (
     <>
+      <HttpPost />
     </>
   )
 }
